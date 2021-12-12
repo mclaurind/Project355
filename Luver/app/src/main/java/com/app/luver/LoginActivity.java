9b354c2d27b0a2377621b82a370ebc302c47cc2b
Darriel Mclaurin <darriel364@gmail.com>
2021-12-12 05:21:08 -0500

    fixed login bug

--- Luver/app/src/main/java/com/app/luver/LoginActivity.java
+++ Luver/app/src/main/java/com/app/luver/LoginActivity.java
@@ -45,7 +45,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         };
 
-        mLogin = (Button) findViewById(R.id.registration);
+        mLogin = (Button) findViewById(R.id.login);
 
         mEmail = (EditText) findViewById(R.id.email);
         mPassword = (EditText) findViewById(R.id.password);
